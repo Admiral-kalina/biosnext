3d4cad872b5a298cbc860e6595028fc3a6a1a56e
Kalinichenko <kalinamono@gmail.com>
2023-10-19 10:03:52 +0300

    fix deploy v1.1

--- src/app/aboutCompany/page.jsx
+++ src/app/aboutCompany/page.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React from "react";
 import Activities from "@/components/Activities/Activities";
 import AboutCompany from "@/components/AboutCompany/AboutCompany";
@@ -6,7 +7,6 @@ import Header from "@/components/Header/Header";
 const AboutCompanyPage = () => {
     return (
         <div className={` page about-page`}>
-
             <AboutCompany/>
         </div>
     );
